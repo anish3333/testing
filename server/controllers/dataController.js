b95e2c123c2409ecdde26cbc74a6a14719b47cf0
Awesohame <82087247+awesohame@users.noreply.github.com>
2025-03-21 03:00:16 +0530

    simulate load testing

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -1,6 +1,13 @@
 exports.receiveData = (req, res) => {
     console.log('Received data:', req.body);
-    res.status(201).json({
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    if (randomNumber > 75) {
+        return res.status(400).json({
+            message: 'Random number is greater than 75',
+            number: randomNumber,
+        });
+    }
+    res.status(200).json({
         message: 'Data received successfully',
         data: req.body,
     });
